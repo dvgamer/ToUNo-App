559dc1a78bd4b77dbbfb6c0a800ec43870b4a6f3
Kananek Thongkam <info.dvgamer@gmail.com>
2016-07-20 00:28:53 +0700

    Add package and update app

---
+++ app.js
@@ -0,0 +1,20 @@
+"use strict";
+// oauth.touno-k.com/sentinel?code=bb42f3a8f7298cd65d7d&state=xxxx
+
+const express = require('express')();
+const http 		= require("http").createServer(express);
+const moment  = require('moment');
+const chalk   = require('chalk');
+const cron 		= require('cron');
+const port		= 8450;
+
+express.use('/travox', [], require("./route/travox-sentinel"));
+
+http.listen(port, ()=>{
+  console.log(`Server OAuth 2.0 [0.0.0.0:${port}] at ${moment().format("HH:mm:ss")} Started`);
+});
+
+process.on('exit', (code)=>{
+  console.log(`Server OAuth 2.0 Shutdown (${code})...`);
+  console.log(`Server OAuth 2.0 at ${moment().format("HH:mm:ss")} Restarting...`);
+});
