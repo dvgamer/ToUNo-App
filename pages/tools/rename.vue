d965f825d4547f28a16d728ae9329861d3934823
Kananek T <info.dvgamer@gmail.com>
2018-04-14 00:38:09 +0700

    add tools

---
+++ pages/tools/rename.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-row>
+    <b-col>
+      <b-breadcrumb :items="breadcrumb"/>
+      <div style="text-align: center">
+        <p>Hi from {{ name }}</p>
+        <nuxt-link to="/">Back</nuxt-link>
+      </div>
+    </b-col>
+  </b-row>
+
+</template>
+
+<script>
+export default {
+  asyncData ({ req }) {
+    return {
+      name: req ? 'server' : 'client'
+    }
+  },
+  data () {
+    return {
+      breadcrumb: [
+        {
+          text: 'Anime',
+          active: true
+        }
+      ]
+    }
+  }
+}
+</script>
